8cfde1131e2e42dcb19045ec2bd8ec695bfd75ff
Connor Votroubek <connorvotroubek@berkeley.edu>
2017-10-19 20:07:23 +0000

    Fixed migration error

--- db/migrate/20171018200733_rename_time_availabilities.rb
+++
@@ -1,9 +0,0 @@
-class RenameTimeAvailabilities < ActiveRecord::Migration
-  def self.up
-    rename_table :time_availabilities, :time_availabilitys
-  end
-
-  def self.down
-    rename_table :time_availabilitys, :time_availabilities
-  end
-end
