07de7ee1124ac39bbb7c1b883e34831bbe8658af
Lexie Polevoi <apolevoi@berkeley.edu>
2017-10-24 19:04:07 +0000

    Fixed factory bot error and started cucumber

--- app/controllers/tutees_controller.rb
+++ app/controllers/tutees_controller.rb
@@ -1,7 +1,10 @@
 class TuteesController < ApplicationController
 
     before_action :validate_params, only: [:update]
-
+    
+    def after_sign_in_path_for(tutee)
+        stored_location_for(tutee) || root
+    end
 
     
     
