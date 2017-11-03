518fe8cdfbdbe59daed60ffbccf077f3c39c52f6
Juno Morey <j.morey@berkeley.edu>
2017-11-03 16:21:16 -0700

    add checkbox

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102200613) do
+ActiveRecord::Schema.define(version: 20171103213754) do
 
   create_table "time_availabilities", force: :cascade do |t|
     t.string   "day"
