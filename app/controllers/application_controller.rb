2a7e0cffa569f76c683b9a94b12be7f8d2448700
Lexie Polevoi <apolevoi@berkeley.edu>
2017-11-22 21:35:09 +0000

    [refactor] Trying to reduce cognitive complexity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,13 +23,18 @@ class ApplicationController < ActionController::Base
             end
         rescue ArgumentError => e
             flash[:error] = e.message
-            if(model_sym == :tutee)
-                redirect_to edit_tutee_path(model)
-            else
-                redirect_to edit_tutor_path(model)
-            end    
+            redirect_to_correct_edit_form(model, model_sym)
                 
         end    
   end
+  
+  def redirect_to_correct_edit_form(model, model_sym)
+    if(model_sym == :tutee)
+      redirect_to edit_tutee_path(model) and return
+    else
+      redirect_to edit_tutor_path(model) and return
+    end  
+  end  
+  
   
 end
